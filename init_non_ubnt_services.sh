ed0d05f526fe6001c6717281c4018cae1faca002
Mike <xenithorb@users.noreply.github.com>
2016-03-10 13:47:52 -0500

    setup non-standard (non-UBNT) services

---
+++ init_non_ubnt_services.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# include
+. include/common_functions
+
+SERVICES=( 
+	"freeradius"
+	"knockd"
+)
+
+for i in "${SERVICES[@]}"; do 
+	if ! get_done "$i"; then
+		update-rc.d "$i" defaults && 
+		
+		if [[ "$i" == "freeradius" ]]; then
+			# Setup the log folder that it expects
+			# Fails otherwise	
+			log_folder="/var/log/freeradius"
+			mkdir -p "$log_folder" &&
+			chown -R freerad. "$log_folder" &&
+			
+			# Make sure symlink is setup for /config/etc/freeradius
+			mv /etc/freeradius{,.old} &&
+			ln -s /config/etc/freeradius /etc/freeradius &&
+			put_done "$i"
+		fi
+
+		if [[ "$i" == "knockd" ]]; then
+			mv /etc/knockd.conf{,.old} &&
+			ln -s /config/etc/knockd.conf /etc/knockd.conf &&
+			put_done "$i"
+		fi
+
+		service "$i" start
+	fi
+done
