8dc595240d7589be53f20861daac3da3adcbd8d2
Mike <xenithorb@users.noreply.github.com>
2016-03-10 13:47:22 -0500

    Install apt packages on startup + common functions

---
+++ 00_install_my_packages.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#include 
+. include/common_functions
+
+packages=(
+	"vim"
+	"tmux"
+	"htop"
+	"dnsutils"
+	"harden-tools"
+	"ngrep"
+	"tcpdump"
+	"man"
+#	"iperf3"
+	"git"
+	"wget"
+	"freeradius" # CRITICAL
+	"knockd" # CRITICAL
+	"apt-utils"
+	"apt-file"
+)
+
+if ! get_done "packages"; then
+	apt-get update &&
+	apt-get install -y "${packages[@]}" && 
+
+	if [[ "$?" == 0 ]]; then 
+		echo "package install successful" 
+		put_done "packages"
+	else
+		echo "package install failed"
+	fi
+fi
